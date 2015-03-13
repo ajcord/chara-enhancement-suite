2862f7353f55c662b3d48fff3149230cf334a405
Thomas Liu <thomasliu02@gmail.com>
2015-03-13 01:37:10 -0500

    Added delete queue to queue page. Fixes #1.

--- data/chara_notify.js
+++ data/chara_notify.js
@@ -1,5 +1,9 @@
 console.log("Children of questions: " + $("#questions").children().toString());
 
+var queue_url = window.location.pathname;
+
+$("#new_lab_queue_staff").append("".concat('<a href="/api', queue_url, '" class="btn btn-danger deleteQuestion" style="width: 100%; margin-top: 2px; margin-left:auto; margin-right: auto;" data-confirm="This will delete the queue and any questions still remaining on it." data-method="delete" data-original-title="Click to delete this queue" data-remote="true" data-type="json" rel="nofollow">Delete Queue</a>'))
+
 $("#sidebar").append('<audio id="sound_notification"> <source src="https://dl.dropboxusercontent.com/u/2758934/wilhelm.ogg" type="audio/ogg"> </audio>');
 
 console.log("Inserted audio.");
