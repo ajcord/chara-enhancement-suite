d2e06458d2b58e6036038dcfbb4ada6eee873a50
Thomas Liu <thomasliu02@gmail.com>
2015-03-13 13:46:13 -0500

    Removed console logs

--- data/chara_notify.js
+++ data/chara_notify.js
@@ -1,8 +1,4 @@
-console.log("Children of questions: " + $("#questions").children().toString());
-
 $("#sidebar").append('<audio id="sound_notification"> <source src="https://dl.dropboxusercontent.com/u/2758934/wilhelm.ogg" type="audio/ogg"> </audio>');
-
-console.log("Inserted audio.");
 
 function getCharaQueueLength() {
     return document.getElementsByTagName("tr").length;
@@ -14,19 +10,13 @@ var baseTitle = document.title;
 
 function refresh() {
 
-    console.log("Mutation detected.");
-    
     if (lastLength < getCharaQueueLength()) {
-	console.log("Previous length: " + lastLength);
-	console.log("Current length: " + getCharaQueueLength());
-	console.log("New question added.");
 	lastLength = getCharaQueueLength();
 	document.getElementById("sound_notification").play();
     }
     
     lastLength = getCharaQueueLength();
     document.title = "".concat("(", (lastLength - 1).toString(), ") ", baseTitle);
-    console.log("Previous length: " + lastLength);
 
     setTimeout(refresh, 1000);
 }
