c965fea1daefeee51211cc3b485b02acfb17dde6
Alex Cordonnier <alexjcord@gmail.com>
2015-04-17 00:20:13 -0500

    Fixed favicon

--- data/insert_favicon.js
+++ data/insert_favicon.js
@@ -6,7 +6,7 @@ chrome.storage.sync.get({
         favicon.rel = "icon";
         favicon.type = "image/png";
         favicon.href = "https://raw.githubusercontent.com/ajcord/chara-enhancement-suite/master/favicon.png";
-        document.childNodes[1].appendChild(favicon);
+        document.head.appendChild(favicon);
         document.title = document.title.replace("---", " - ");
     }
 });
